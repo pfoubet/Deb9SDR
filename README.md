e0829b9f047452f0eb44299605b9da1c7ecdbef6
Patrick H. E. Foubet <pf@taveeg>
2018-11-29 14:31:56 +0100

    Version 0.1

---
+++ README.md
@@ -0,0 +1,6 @@
+#
+# VM Debian 9.5.0 Software Defined Radio
+#
+# Cours de P. Foubet
+#
+
---
+++ recolle.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+cat Deb9SDRaa Deb9SDRab Deb9SDRac Deb9SDRad Deb9SDRae Deb9SDRaf Deb9SDRag Deb9SDRah Deb9SDRai Deb9SDRaj Deb9SDRak Deb9SDRal Deb9SDRam Deb9SDRan Deb9SDRao Deb9SDRap Deb9SDRaq Deb9SDRar Deb9SDRas Deb9SDRat Deb9SDRau Deb9SDRav Deb9SDRaw Deb9SDRax Deb9SDRay Deb9SDRaz Deb9SDRba Deb9SDRbb Deb9SDRbc Deb9SDRbd Deb9SDRbe Deb9SDRbf Deb9SDRbg Deb9SDRbh Deb9SDRbi Deb9SDRbj Deb9SDRbk Deb9SDRbl Deb9SDRbm Deb9SDRbn Deb9SDRbo Deb9SDRbp Deb9SDRbq Deb9SDRbr Deb9SDRbs Deb9SDRbt Deb9SDRbu > SdrDeb9.ova
+
+
+
+ 
